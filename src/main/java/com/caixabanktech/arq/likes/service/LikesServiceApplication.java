8d13db0910f1fa966ff79913f94aaaf779f4d4f6
Francisco M. Palos Barcos <fmpalos@caixabanktech.com>
2023-05-25 10:06:16 +0200

    Initial commit

---
+++ src/main/java/com/caixabanktech/arq/likes/service/LikesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.caixabanktech.arq.likes.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caixabanktech/arq/likes/service/LikesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caixabanktech.arq.likes.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
